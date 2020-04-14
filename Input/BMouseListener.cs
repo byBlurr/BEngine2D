fa5b0c4d43e305ee696787f192dfee5c959c7416
byBlurr <dbldumont11@gmail.com>
2020-04-14 06:58:55 +0100

    Move control enums

--- Input/BMouseListener.cs
+++ Input/BMouseListener.cs
@@ -32,4 +32,21 @@ namespace BEngine2D.Input
             ButtonStates[(int)button].Location = location;
         }
     }
+
+    public enum BMouseButton
+    {
+        Left = 0,
+        Middle = 1,
+        Right = 2,
+        Button1 = 3,
+        Button2 = 4,
+        Button3 = 5,
+        Button4 = 6,
+        Button5 = 7,
+        Button6 = 8,
+        Button7 = 9,
+        Button8 = 10,
+        Button9 = 11,
+        LastButton = 12
+    }
 }
