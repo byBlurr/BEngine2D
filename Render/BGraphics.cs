af75012097862471e025d2a99f09ad56c808efc6
byBlurr <dbldumont11@gmail.com>
2020-04-14 06:09:06 +0100

    Move LoadTexture into BGraphics
    Removed the BContentPipe class

--- Render/BGraphics.cs
+++ Render/BGraphics.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
 using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace BEngine2D.Render
 {
@@ -50,6 +52,41 @@ namespace BEngine2D.Render
             GL.LoadIdentity();
 
             GL.Ortho(-screenWidth / 2.0f, screenWidth / 2.0f, screenHeight / 2.0f, -screenHeight / 2.0f, 0.0f, 1.0f);
+        }
+
+        public static BTexture LoadTexture(string path)
+        {
+            if (!File.Exists("Content/" + path)) throw new FileNotFoundException($"File not found at 'Content/{path}'.");
+
+            int id = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2D, id);
+
+            Bitmap bmp = new Bitmap("Content/" + path);
+            BitmapData data = bmp.LockBits(
+                new Rectangle(0, 0, bmp.Width, bmp.Height),
+                ImageLockMode.ReadOnly,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb
+            );
+
+            GL.TexImage2D(
+                TextureTarget.Texture2D,
+                0,
+                PixelInternalFormat.Rgba,
+                data.Width, data.Height,
+                0,
+                OpenTK.Graphics.OpenGL.PixelFormat.Bgra,
+                PixelType.UnsignedByte,
+                data.Scan0
+            );
+
+            bmp.UnlockBits(data);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Clamp);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Clamp);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+            return new BTexture(id, bmp.Width, bmp.Height);
         }
     }
 }
