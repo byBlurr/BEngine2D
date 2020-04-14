f8ba7960a5e05e5cb60f1690c1e02f31d3882723
byBlurr <dbldumont11@gmail.com>
2020-04-14 09:32:15 +0100

    Render level in BGameState

--- GameStates/BGameState.cs
+++ GameStates/BGameState.cs
@@ -1,11 +1,17 @@
 ﻿using BEngine2D.Render;
+using BEngine2D.Util;
+using BEngine2D.World;
 using BEngine2D.World.Blocks;
+using System.Drawing;
+using System.Numerics;
 
 namespace BEngine2D.GameStates
 {
     public class BGameState : BState
     {
+        protected BLevel Level;
         protected BCamera Camera;
+        protected BTexture[] Textures = new BTexture[255];
 
         public override void OnLoad()
         {
@@ -23,8 +29,16 @@ namespace BEngine2D.GameStates
         public override void Draw()
         {
             Camera.ApplyTransform();
-            // draw the mouse icon
             base.Draw();
+
+            for (int x = 0; x < Level.Width; x++)
+            {
+                for (int y = 0; y < Level.Height; y++)
+                {
+                    RectangleF source = Level[x, y].TexturePosition;
+                    BGraphics.Draw(Textures[0], new Vector2(x * AppInfo.GRIDSIZE, y * AppInfo.GRIDSIZE), new Vector2((float)AppInfo.GRIDSIZE / AppInfo.TILESIZE), Color.Transparent, Vector2.Zero, source);
+                }
+            }
         }
     }
 }
